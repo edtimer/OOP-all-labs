ab566e77883a0513d5609930b41b64031a10153c
edtimer <26600954+edtimer@users.noreply.github.com>
2021-06-04 12:55:08 +0100

    health info class

---
+++ lab7/HealthInfo.java
@@ -0,0 +1,19 @@
+public class HealthInfo {
+
+
+    private int height;
+    private int weight;
+
+    public HealthInfo(int height, int weight){
+        this.height = height;
+        this.weight =weight;
+    }
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+}
